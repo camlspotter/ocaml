0455aff2310a745273227f44611abfec72c41790
Jun FURUSE <jun.furuse@gmail.com>
2014-11-05 18:21:15 +0800

    merged back ppx_pattern_guard

--- testsuite/pattern_guard/test.ml
+++ testsuite/pattern_guard/test.ml
@@ -1,3 +1,4 @@
+(*
 let _ =
   match 3, 2 with
   | (x, y) when x = y -> prerr_endline "0"
@@ -6,6 +7,7 @@ let _ =
   | (x, y) with w <- x + y when w = 5 -> prerr_endline "3"
   | (x, y) when x = y + 1 when x = y + 1 -> prerr_endline "4"
   | _ -> prerr_endline "_"
+*)
 
 let _ =
   match 3, 2 with
@@ -18,11 +20,22 @@ let _ =
 
 let () = 
   let f = function
-    | (x, y) when x = y -> prerr_endline "0"
-    | (x, y) with w <- x + y when w = 4 -> prerr_endline "1"
-    | (x, y) with Some x <- None -> prerr_endline x
-    | (x, y) with w <- x + y when w = 5 -> prerr_endline "3"
-    | (x, y) when x = y + 1 when x = y + 1 -> prerr_endline "4"
+    | (x, y) when [%guard x = y] -> prerr_endline "0"
+    | (x, y) when [%guard let w = x + y;; w = 4] -> prerr_endline "1"
+    | (x, y) when [%guard let Some x = None]  -> prerr_endline x
+    | (x, y) when [%guard let w = x + y;; w = 5] -> prerr_endline "3"
+    | (x, y) when [%guard x = y + 1;; x = y + 1] -> prerr_endline "4"
     | _ -> prerr_endline "_"
   in
-  f (3,2)
+  f (3, 2)
+
+exception E of int * int
+
+let () = 
+  try raise (E (3, 2)) with
+  | E (x, y) when [%guard x = y] -> prerr_endline "0"
+  | E (x, y) when [%guard let w = x + y;; w = 4] -> prerr_endline "1"
+  | E (x, y) when [%guard let Some x = None]  -> prerr_endline x
+  | E (x, y) when [%guard let w = x + y;; w = 5] -> prerr_endline "3"
+  | E (x, y) when [%guard x = y + 1;; x = y + 1] -> prerr_endline "4"
+  | E _ -> prerr_endline "_"
