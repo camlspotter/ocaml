f7daf9718583dc59213386c1f12036ef29f35ba1
Jun Furuse <jun.furuse@gmail.com>
2017-10-25 14:11:13 +0000

    letopardlib test fixes

--- leopardlib/test.ml
+++ leopardlib/test.ml
@@ -3,13 +3,8 @@ open Leopard.Overload
 
 let () =
   assert ((+) 1 2 = 3);
-(*
   assert (1.2 + 3.4 = 4.6);
-*)
-(*
   Printf.printf "%c\n" ("hello".[0]);
   Printf.printf "%d\n" ([|1;2;3|].[0]);
-*)
   ()
-  
     
