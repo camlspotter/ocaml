e722a0d5f90a5a3d0428658432635202dcec8986
Jun Furuse <jun.furuse@gmail.com>
2017-10-30 16:55:02 +0000

    Moved tool functions to Leopardtype

--- typing/overload.ml
+++ typing/overload.ml
@@ -2,7 +2,8 @@
 open Asttypes
 open Types
 open Typedtree
-
+open Leopardtype
+    
 (*
 let print_ident ppf id = Format.fprintf ppf "%s/%d" id.Ident.name id.Ident.stamp
 
@@ -12,55 +13,6 @@ let rec print_path ppf = function
   | Path.Papply (p1, p2) -> Format.fprintf ppf "%a(%a)" print_path p1 print_path p2
 *)
 
-let scrape_sg env mdecl = 
-  try
-    match Env.scrape_alias env @@ Mtype.scrape env mdecl.Types.md_type with
-    | Mty_signature sg -> sg
-    | Mty_functor _ -> [] (* We do not scan the internals of functors *)
-    | _ -> assert false
-  with
-  | e -> 
-      Format.eprintf "scraping failed: %s" @@ Printexc.to_string e;
-      raise e
-  
-let fold_module env path init f =
-  Format.eprintf "fold_module %a@." Printtyp.path path;
-  let mdecl = Env.find_module path env in
-  let mty = mdecl.Types.md_type in
-  let sg : Types.signature = scrape_sg env mdecl in
-  let id = Ident.create "Dummy" in
-  let env' = Env.add_module id mty Env.empty in
-  let lid id = Longident.(Ldot (Lident "Dummy", id.Ident.name)) in
-  let pathfix p = match p with
-    | Path.Pdot (Path.Pident _, s, n) -> Path.(Pdot (path, s, n))
-    | _ -> assert false
-  in
-  List.fold_left (fun st i ->
-      let x = match i with
-        | Sig_value (id, _vdesc) ->
-            let p, vdesc = Env.lookup_value (lid id) env' in
-            `Value (id, pathfix p, vdesc)
-        | Sig_type (id, td, _) ->
-            let p = Env.lookup_type (lid id) env' in
-            `Type (id, pathfix p, td)
-        | Sig_typext (id, ec, _) ->
-            let p = Env.lookup_type (lid id) env' in
-            `Typext (id, pathfix p, ec)
-        | Sig_module (id, mdecl, _) ->
-            let p = Env.lookup_module ~load:false (lid id) env' in
-            `Module (id, pathfix p, mdecl)
-        | Sig_modtype (id, _) ->
-            let p, mdtd = Env.lookup_modtype (lid id) env' in
-            `Modtype (id, pathfix p, mdtd)
-        | Sig_class (id, _, _) ->
-            let p, cd = Env.lookup_class (lid id) env' in
-            `Class (id, pathfix p, cd)
-        | Sig_class_type (id, _, _) ->
-            let p, ctd = Env.lookup_cltype (lid id) env' in
-            `Cltype (id, pathfix p, ctd)
-      in
-      f st x) init sg
-  
 let get_name = function
   | Path.Pident id -> Ident.name id
   | Path.Pdot (_, name, _) -> name
@@ -74,7 +26,9 @@ let test env ty vdesc =
   res
 
 let resolve_overloading exp ({loc} as lidloc) path = 
+(*
   Format.eprintf "resolve_overloading %a %a@." !Location.printer loc Printtyp.path path;
+*)
   let env = exp.exp_env in
 
   let name = get_name path in
