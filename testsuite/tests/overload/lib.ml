182a34a5d3a56030fb00589d33ea86028f0bbbb7
Jun Furuse <jun.furuse@gmail.com>
2017-10-30 06:55:30 +0000

    added external overloading test

---
+++ testsuite/tests/overload/lib.ml
@@ -0,0 +1,18 @@
+module Overload = struct:
+
+  val %overload (+)   : 'a -> 'a -> 'a
+  val %overload (-)   : 'a -> 'a -> 'a
+  val %overload (/)   : 'a -> 'a -> 'a
+  val %overload ( * ) : 'a -> 'a -> 'a
+  
+  module Int = struct:
+    let (+) = (+)
+    let (-) = (-)
+    let ( * ) = ( * )
+    let (/) = (/)
+  
+  module Float = struct:
+    let (+) = (+.)
+    let (-) = (-.)
+    let ( * ) = ( *. )
+    let (/) = (/.)
