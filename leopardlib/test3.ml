93c98af1cf3614e9c723c3090cd2fc5412272929
Jun Furuse <jun.furuse@gmail.com>
2017-11-06 08:24:10 +0000

    Retyping is not a good idea... Found the natture of the issue of ungeneralized let double ?d x = add ?d x x
    We cannot perform Ctype.unify easily,
    which may change the levels of generalized type variables.
    
    After type inference finishes, we often have ill-formed types ty['a]
    in type annotations, where 'a is generalized, but ty is not.
    
    Unification beween such ty['a] with a completely generalized type ty'
    makes 'a not generalized. Because of the ungeneralized type level of ty.

--- leopardlib/test3.ml
+++ leopardlib/test3.ml
@@ -7,9 +7,7 @@ type 'a add = ('a -> 'a -> 'a, [%imp Add]) Leopard.Implicits.t
 
 val %imp add : ?d:'a add -> 'a -> 'a -> 'a
 
+(* If we perform unifications in implicit.ml without thinking, 
+   somehow the type level of double is changed *)
 let double ?d x = add ?d x x
-
-let () =
-  assert (double 1 = 2);
-  assert (double 1.2 = 2.4)
-
+    
