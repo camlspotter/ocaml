5a7944715ff5a65d905ae043bd6120105e7276ec
Jun Furuse <jun.furuse@gmail.com>
2017-12-15 12:54:02 +0000

    added tests/leopard-implicits

---
+++ testsuite/tests/leopard-implicits/t02ok.ml
@@ -0,0 +1,11 @@
+type 'a add = ('a -> 'a -> 'a, [%imp Add]) Leopard.Implicits.t
+
+val %imp add : ?d:'a add -> 'a -> 'a -> 'a
+
+(* If we perform unifications in implicit.ml without thinking, 
+   somehow the type level of double is changed *)
+let double ?d x = add ?d x x
+(*
+let double2 ~d x = add ~d x x
+let double3 d x = add ?d x x
+*)
