d0f99176f17ddde3733be098f980cabe6cdcb241
Jun Furuse <jun.furuse@gmail.com>
2017-12-18 01:29:37 +0000

    Added test6.ml

--- leopardlib/test3.ml
+++ leopardlib/test3.ml
@@ -5,7 +5,3 @@ val %imp add : ?d:'a add -> 'a -> 'a -> 'a
 (* If we perform unifications in implicit.ml without thinking, 
    somehow the type level of double is changed *)
 let double ?d x = add ?d x x
-(*
-let double2 ~d x = add ~d x x
-let double3 d x = add ?d x x
-*)
---
+++ leopardlib/test6.ml
@@ -0,0 +1,18 @@
+type 'a add = ('a -> 'a -> 'a, [%imp Add]) Leopard.Implicits.t
+
+val %imp add : ?d:'a add -> 'a -> 'a -> 'a
+
+module Add = struct:
+  let int = (+)
+
+let r = ref []
+
+let f = add (List.hd !r) (List.hd !r)
+
+(* The resolver chooses `Add.int` for `add` since there is only one candidate.
+
+   BUG: Apart from it is ok or not, the resolution result is never reflected to 
+   the type of `r`. 
+*)
+
+   
