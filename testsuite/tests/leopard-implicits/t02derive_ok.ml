a3f1afd0e3c835ebbdf4f8117868398c0a796926
Jun Furuse <jun.furuse@gmail.com>
2018-02-02 04:37:52 +0000

    label fixes

--- testsuite/tests/leopard-implicits/t02derive_ok.ml
+++ testsuite/tests/leopard-implicits/t02derive_ok.ml
@@ -1,11 +1,11 @@
 type 'a add = ('a -> 'a -> 'a, [%imp just Add]) Leopard.Implicits.t
 
-val %imp add : ?d:'a add -> 'a -> 'a -> 'a
+val %imp add : ?_d:'a add -> 'a -> 'a -> 'a
 
 (* If we perform unifications in implicit.ml without thinking, 
    somehow the type level of double is changed *)
-let double ?d x = add ?d x x
+let double ?_d x = add ?_d x x
 (*
 let double2 ~d x = add ~d x x
-let double3 d x = add ?d x x
+let double3 d x = add ?_d x x
 *)
--- testsuite/tests/leopard-implicits/t17adm_bad.ml
+++ testsuite/tests/leopard-implicits/t17adm_bad.ml
@@ -1,12 +1,12 @@
 type 'a show = ('a -> string, [%imp just Show]) Leopard.Implicits.t
 
-val %imp show : ?d:'a show -> 'a -> string
+val %imp show : ?_d:'a show -> 'a -> string
   
 module Show = struct
   (* Skip this candidate because of non decreasing %imp recursive dependency:
      <= The message is only printed in the debug. We should output it as a warning.
   *) 
-  let broken : ?d:'a list show -> 'a -> string = fun ?d x -> show ?d [x]
+  let broken : ?_d:'a list show -> 'a -> string = fun ?_d x -> show ?_d [x]
 end
 
 let () = assert (show 1 = "1")
