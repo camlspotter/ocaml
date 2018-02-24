98c8a66e98119ad367c8b3a7b0909a6c2d5b7993
Jun Furuse <jun.furuse@gmail.com>
2018-02-24 09:15:27 +0000

    tests are moved

--- testsuite/tests/leopard-implicits/t30resolve_unify_ok.ml
+++ testsuite/tests/leopard-implicits/t30resolve_unify_ok.ml
@@ -1,6 +1,6 @@
 type 'a add = ('a -> 'a -> 'a, [%imp just Add]) Leopard.Implicits.t
 
-val %imp add : ?d:'a add -> 'a -> 'a -> 'a
+val %imp add : _d:'a add -> 'a -> 'a -> 'a
 
 module Add = struct:
   let int = (+)
@@ -11,6 +11,6 @@ let f = add (List.hd !r) (List.hd !r)
 
 (* The resolver chooses `Add.int` for `add` since there is only one candidate.
 
-   BUG (already fixed): Apart from it is ok or not, the resolution result is never reflected to 
-   the type of `r`. 
+   BUG (already fixed): Apart from it is ok or not, the resolution result 
+     was never reflected to the type of `r`. 
 *)
