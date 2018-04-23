82ec7c90edd500d1759ed0bf4d7f992fdefff935
Jun Furuse <jun.furuse@gmail.com>
2018-04-23 02:20:21 +0000

    simplified of deriving candidate

--- testsuite/tests/leopard-implicits/t37_imp_exp_ok.ml
+++ testsuite/tests/leopard-implicits/t37_imp_exp_ok.ml
@@ -1,9 +1,3 @@
-(* simple overloading *)
 let add ~_d:(_d:('a, 'b) Leopard.Implicits.t) = Leopard.Implicits.get ~_d
-
-(* =>
-Error: This type 'b is where a type of encoded string was expected 
-({id=854;level=100000000;desc= Tvar "b"})
-
-Hmm, this is not very very required...
-*)
+(* 'b is not a valid spec type, but it should be accepted *)
+    
